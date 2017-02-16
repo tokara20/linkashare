42c5908eab14fd8f9fe203158ec4a71a0fd3615a
michael-palad <happyfighter2015@gmail.com>
2017-02-16 18:49:17 +0000

    add links controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def sign_up_text
+    raw('<span class="glyphicon glyphicon-user"></span> Sign Up')
+  end
+  
+  def login_text
+    raw('<span class="glyphicon glyphicon-log-in"></span> Login')
+  end
+  
+  def logout_text
+    raw('<span class="glyphicon glyphicon-log-out"></span> Logout')
+  end
+  
+  def edit_profile_text  
+    raw('<span class="glyphicon glyphicon-user"></span> Edit Profile')  
+  end
 end
