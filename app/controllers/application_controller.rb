759c2bead5ea16acf155b9b28bcc1f244ff65e30
michael-palad <happyfighter2015@gmail.com>
2017-02-16 19:52:56 +0000

    modify views and add user profile image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, 
+        keys: [:username, :profile_image])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_attached_file :profile_image, styles: { medium: "300x300>", 
+      thumb: "100x100>", micro: "50x50>" }, 
+      default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
 end
