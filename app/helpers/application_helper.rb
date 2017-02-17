e771b891b9fcc216539e5a7a5f2a1fd560d6288d
michael-palad <happyfighter2015@gmail.com>
2017-02-17 02:55:06 +0000

    modify navbar buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,26 @@
 module ApplicationHelper
   def new_link_text
-    raw('<span class="glyphicon glyphicon-link"></span> New Link')
+    #raw('<span class="glyphicon glyphicon-link"></span> New Link')
+    'New Link'
   end
   
   def sign_up_text
-    raw('<span class="glyphicon glyphicon-user"></span> Sign Up')
+    #raw('<span class="glyphicon glyphicon-user"></span> Sign Up')
+    'Sign Up'
   end
   
   def login_text
-    raw('<span class="glyphicon glyphicon-log-in"></span> Login')
+    #raw('<span class="glyphicon glyphicon-log-in"></span> Login')
+    'Login'
   end
   
   def logout_text
-    raw('<span class="glyphicon glyphicon-log-out"></span> Logout')
+    #raw('<span class="glyphicon glyphicon-log-out"></span> Logout')
+    'Logout'
   end
   
   def edit_profile_text  
-    raw('<span class="glyphicon glyphicon-user"></span> Edit Profile')  
+    #raw('<span class="glyphicon glyphicon-user"></span> Edit Profile')  
+    'Edit Profile'
   end
 end
