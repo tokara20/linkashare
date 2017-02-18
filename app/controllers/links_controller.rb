35b863354f0811368777273dcf8dcb591afe6527
michael-palad <happyfighter2015@gmail.com>
2017-02-18 16:11:12 +0000

    add error handling in adding links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -14,29 +14,39 @@ class LinksController < ApplicationController
       format.js do
         if params[:fetch_url]
           # add url checking
-          link_data = LinkThumbnailer.generate(link_params[:url])
+          @link = Link.new
+          begin
+            link_data = LinkThumbnailer.generate(link_params[:url])
+          rescue
+            @link.errors.add(:url, "Unable to reach link.")
+            render 'new' and return
+          end
         
           # fill in other fields
-          @link = Link.new
           @link.url = link_params[:url]
           @link.title = link_data.title
           @link.description = link_data.description
         elsif params[:add_link]
           link_data = LinkThumbnailer.generate(link_params[:url])
          
+          byebug
           # save thumbnail to image_path
-          image = open(link_data.images.first.src.to_s)
-          filename = image.base_uri.to_s.split('/')[-1]
-          image_path = Rails.root.join('tmp', filename)   
-          IO.copy_stream(image, image_path) 
+          unless link_data.images.empty?
+            image = open(link_data.images.first.src.to_s)
+            filename = image.base_uri.to_s.split('/')[-1]
+            image_path = Rails.root.join('tmp', filename)   
+            IO.copy_stream(image, image_path)
+          end
         
           # fill in other fields
           @link = Link.new
           @link.url = link_params[:url]
           @link.title = link_params[:title]
           @link.description = link_params[:description]
-          File.open(image_path, 'rb') do |file|
-            @link.website_image = file
+          unless link_data.images.empty?
+            File.open(image_path, 'rb') do |file|
+              @link.website_image = file
+            end
           end
           @link.user = current_user
           
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ApplicationRecord
 
   has_attached_file :website_image, styles: { medium: "300x300>", thumb: "100x100>" },
-        default_url: "/images/:style/missing.png"
+        default_url: "/images/missing.png"
   validates_attachment_content_type :website_image, content_type: /\Aimage\/.*\z/
   
   # Associations
