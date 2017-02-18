ad6fcfa46e7adec71db7fc80b3783a4925af9a12
michael-palad <happyfighter2015@gmail.com>
2017-02-18 18:14:19 +0000

    adjusted styling and add submitter to show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,21 @@
 module ApplicationHelper
   def new_link_text
-    #raw('<span class="glyphicon glyphicon-link"></span> New Link')
     'New Link'
   end
   
   def sign_up_text
-    #raw('<span class="glyphicon glyphicon-user"></span> Sign Up')
     'Sign Up'
   end
   
   def login_text
-    #raw('<span class="glyphicon glyphicon-log-in"></span> Login')
     'Login'
   end
   
   def logout_text
-    #raw('<span class="glyphicon glyphicon-log-out"></span> Logout')
     'Logout'
   end
   
   def edit_profile_text  
-    #raw('<span class="glyphicon glyphicon-user"></span> Edit Profile')  
     'Edit Profile'
   end
 end
