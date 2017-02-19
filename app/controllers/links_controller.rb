1c94677a2867109f009924d0843ab141129ad12f
michael-palad <happyfighter2015@gmail.com>
2017-02-19 12:17:31 +0000

    refactor create link action by moving code to model instead

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,7 +10,7 @@ class LinksController < ApplicationController
   end
   
   def my_links
-    @links = Link.where(user: current_user)
+    @links = Link.where(user: current_user).order(created_at: :desc)
     render 'index'
   end
   
@@ -21,41 +21,23 @@ class LinksController < ApplicationController
   def create
     respond_to do |format|
       format.js do
+        @link = Link.new
+        link_data = @link.get_link_data(link_params[:url])
+        unless link_data
+          render 'new' and return
+        end
         if params[:fetch_url]
-          # add url checking
-          @link = Link.new
-          begin
-            link_data = LinkThumbnailer.generate(link_params[:url])
-          rescue
-            @link.errors.add(:url, "Unable to reach link.")
-            render 'new' and return
-          end
-        
-          # fill in other fields
           @link.url = link_params[:url]
           @link.title = link_data.title
           @link.description = link_data.description
         elsif params[:add_link]
-          link_data = LinkThumbnailer.generate(link_params[:url])
-         
           # save thumbnail to image_path
-          unless link_data.images.empty?
-            image = open(link_data.images.first.src.to_s)
-            filename = image.base_uri.to_s.split('/')[-1]
-            image_path = Rails.root.join('tmp', filename)   
-            IO.copy_stream(image, image_path)
-          end
+           
         
-          # fill in other fields
-          @link = Link.new
           @link.url = link_params[:url]
+          @link.fetch_website_image(link_data)
           @link.title = link_params[:title]
           @link.description = link_params[:description]
-          unless link_data.images.empty?
-            File.open(image_path, 'rb') do |file|
-              @link.website_image = file
-            end
-          end
           @link.user = current_user
           
           if @link.save
--- app/models/link.rb
+++ app/models/link.rb
@@ -6,4 +6,25 @@ class Link < ApplicationRecord
   
   # Associations
   belongs_to :user
+  
+  def get_link_data(url)
+    begin
+      return LinkThumbnailer.generate(url)
+    rescue
+      self.errors.add(:url, "Unable to reach link.")
+      return false
+    end
+  end
+  
+  def fetch_website_image(link_data)
+    unless link_data.images.empty?
+      image = open(link_data.images.first.src.to_s)
+      filename = image.base_uri.to_s.split('/')[-1]
+      image_path = Rails.root.join('tmp', filename)   
+      IO.copy_stream(image, image_path)
+      File.open(image_path, 'rb') do |file|
+        self.website_image = file
+      end
+    end
+  end
 end
