195bcb2d4cd41de4e84851c6b4ec347193c1dae1
michael-palad <happyfighter2015@gmail.com>
2017-03-25 15:02:05 +0000

    modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,11 +6,9 @@ set :deploy_to, '/home/deploy/linkashare'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-set :rails_env, fetch(:stage)
-
 namespace :deploy do
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    execute "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=production"
   end
 end
