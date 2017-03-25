73eded8fc459db20b5bd64a959b8653a645f3cfc
michael-palad <happyfighter2015@gmail.com>
2017-03-25 15:45:34 +0000

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, "git@github.com:tokara20/linkashare.git"
 
 set :deploy_to, '/home/deploy/linkashare'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/cloudinary.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 namespace :deploy do
