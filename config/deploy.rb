f2f16812ca8dbe2b5e58fdc2ac244bf27532da13
michael-palad <happyfighter2015@gmail.com>
2017-03-25 15:20:46 +0000

    modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,14 @@ set :deploy_to, '/home/deploy/linkashare'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-desc 'Runs rake db:seed'
-task :seed => [:set_rails_env] do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "db:seed"
+namespace :deploy do
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
       end
     end
   end
